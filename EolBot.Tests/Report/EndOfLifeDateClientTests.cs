bf7fdab266beb8b5df547cd98a9a5c504021ff7c
Artem Gorobets <avorobei@outlook.com>
2025-11-13 16:14:00 +0700

    Add integration tests

---
+++ EolBot.Tests/Report/EndOfLifeDateClientTests.cs
@@ -0,0 +1,34 @@
+﻿using EolBot.Services.Report.Provider.EndoflifeDate.Api;
+
+namespace EolBot.Tests.Report
+{
+    public class EndOfLifeDateClientTests : IDisposable
+    {
+        private readonly HttpClient _httpClient;
+        private readonly EndOfLifeDateClient _client;
+
+        public EndOfLifeDateClientTests()
+        {
+            _httpClient = new()
+            {
+                BaseAddress = new Uri("https://endoflife.date/api/v1/")
+            };
+            _client = new EndOfLifeDateClient(_httpClient);
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public async Task GetFullProductsAsync_ShouldReturnData()
+        {
+            var actual = await _client.GetFullProductsAsync();
+            Assert.True(actual?.Total > 0);
+            Assert.Equal(actual?.Total, actual?.Result.Count());
+        }
+
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
